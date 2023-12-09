20cddc11057c2d025fa8829d5f36a3e7b5330b0f
Felipe Amorim <felipe21fab@gmail.com>
2023-12-09 11:02:31 -0800

    Add eagle moves up and down

--- Assets/Scripts/Eagle.cs
+++ Assets/Scripts/Eagle.cs
@@ -4,4 +4,41 @@ using UnityEngine;
 
 public class Eagle : Enemy
 {
+    [SerializeField] private float upperCap; // Limite superior para o movimento vertical
+    [SerializeField] private float lowerCap; // Limite inferior para o movimento vertical
+
+    [SerializeField] private float jumpHeight = 15f;
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private bool movingUp = true;
+
+    protected override void Start()
+    {
+        base.Start();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // Movimento vertical
+        if (movingUp)
+        {
+            rb.velocity = new Vector2(0, moveSpeed);
+
+            if (transform.position.y >= upperCap)
+            {
+                movingUp = false;
+            }
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, -moveSpeed);
+
+            if (transform.position.y <= lowerCap)
+            {
+                movingUp = true;
+            }
+        }
+    }
 }
